7ef39dabc5a7cdaafb855fbd1dea537a8d30cdff
Joseph Xu <josephxu@Josephs-MacBook-Pro.local>
2021-02-02 10:43:20 -0800

    enable error redirect and static page to show registe successfully

---
+++ server/middleware/url-not-found-handler.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function () {
+    //4XX - URLs not found
+    return function customRaiseUrlNotFoundError(req, res, next) {
+        res.redirect("https://membership.businesstimes.com.hk")
+        // res.sendFile('index.html', function (err) {
+        //     if (err) {
+        //         console.error(err);
+        //         res.status(err.status).end();
+        //     }
+        // });
+    };
+};
