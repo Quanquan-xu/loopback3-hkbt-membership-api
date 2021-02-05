f94f597110c18f6613098962e17bfb6addb4dd15
Joseph Xu <josephxu@Josephs-MacBook-Pro.local>
2021-02-04 16:06:45 -0800

    update model sql command

--- membership.sql
+++ membership.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS member (
     education VARCHAR(255) NOT NULL DEFAULT '',
     gender CHAR(10) NOT NULL DEFAULT '',
     favourites VARCHAR(255) NOT NULL DEFAULT '',
+    notification BOOLEAN NOT NULL DEFAULT FALSE,
     realm VARCHAR(255) NOT NULL DEFAULT '',
     emailVerified BOOLEAN NOT NULL DEFAULT FALSE,
     verificationToken VARCHAR(255) DEFAULT NULL,
@@ -27,7 +28,7 @@ CREATE TABLE IF NOT EXISTS favourite (
     status BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE Role (
+CREATE TABLE IF NOT EXISTS Role (
   id int(11) NOT NULL AUTO_INCREMENT,
   name char(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   description char(100) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -37,7 +38,7 @@ CREATE TABLE Role (
   UNIQUE KEY Role_id_uindex (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE RoleMapping (
+CREATE TABLE IF NOT EXISTS RoleMapping (
   id int(11) NOT NULL AUTO_INCREMENT,
   principalType char(50) COLLATE utf8_unicode_ci NOT NULL,
   principalId int(11) DEFAULT NULL,
